340ee7e5842c75f57c28606b476582de0be20b64
Luis Fernando Ruiz <99106138+RuizL25@users.noreply.github.com>
2025-10-31 00:44:47 -0500

    Implementation of crashlytics to answer BQ

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     //alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
